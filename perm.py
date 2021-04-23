969820e9f16aa5ae6574d93d3c00d55376189ca0
dogacgt <doganuraydogan@gmail.com>
2021-04-23 14:01:54 +0300

    add files

---
+++ perm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Enumerating Gene Orders
+
+import itertools
+import math
+
+def main():
+    iterable = range(1, n+1)
+    with open('output.txt', 'w') as f:
+        f.write(str(math.factorial(n)) + '\n')
+        result = itertools.permutations(iterable, n)
+        for item in result:
+            f.write(' '.join(map(str, item)) + '\n')
+
+if __name__ == '__main__':
+    with open('data/rosalind_perm.txt') as f:
+        n = int(f.read().strip())
+        main()
---
+++ pper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+
+def main():
+    result = 1
+    ways = range(1, n+1)
+    for j in range(1, k+1):
+        result *= ways[-j]
+    return result % 10**6
+
+if __name__ == '__main__':
+    with open('data/rosalind_pper.txt', 'r') as f:
+        n, k = map(int, f.read().strip().split())
+        print(main())
