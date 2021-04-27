a5a1ed0720928814a5deca400d9a7b8a936b34cf
dogacgt <doganuraydogan@gmail.com>
2021-04-27 22:05:47 +0300

    update

--- indc.py
+++ indc.py
@@ -2,17 +2,12 @@
 #Independent Segregation of Chromosomes
 
 from math import factorial, log
-
-with open('data/rosalind_indc.txt') as f:
-    n = int(f.read().strip())
-
-arr = [0]*(n*2)
 #common log: log_10(probability)
 
-def binomDist(n, arr):
+def binom_dist(n):
+    arr = [0]*(n*2)
     ind = 2 * n
     binom = 0
-    sharingChr = 1/(ind)
     for i in range(ind, 0, -1):
         binom += C(ind, i) * (.5 ** (ind))
         arr[i-1] = binom
@@ -20,9 +15,12 @@ def binomDist(n, arr):
 
     with open('output.txt', 'w') as f:
         arr = ['%.4f' % a for a in arr]
-        f.write(' '.join(map(str, arr)))
+        f.write(' '.join(arr))
 
 def C(m, r):
     return factorial(m)/(factorial(m-r) * factorial(r))
 
-binomDist(n, arr)
+if __name__ == '__main__':
+    with open('data/rosalind_indc.txt') as f:
+        n = int(f.read().strip())
+    binom_dist(n)
