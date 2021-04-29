75e0a6931cf482f73088aa9043a14c8ccc1154bf
dogacgt <doganuraydogan@gmail.com>
2021-04-29 11:13:01 +0300

    update

--- deg.py
+++ deg.py
@@ -2,15 +2,8 @@
 #Degree Array
 
 with open('data/rosalind_deg.txt', 'r') as f:
-    f = f.readlines()
-    myList = []
-    for line in f:
-        line = line.rstrip('\n').split(' ')
-        for i in line:
-            myList.append(int(i))
-
-newList = [i for i in range(1, myList[0]+1)]
+    res = list(map(int, f.read().strip().split()))
 
 with open('output.txt', 'w') as f:
-    for i in newList:
-        f.write(str(myList[2:].count(i)) + ' ')
+    for i in range(1, res[0]+1):
+        f.write('{} '.format(str(res[2:].count(i))) )
--- kmer.py
+++ kmer.py
@@ -4,21 +4,23 @@
 import re
 import itertools
 
-with open('data/rosalind_kmer.txt') as f:
-    f = f.readlines()
-    s = ''
-    for line in f:
-        if line[0] != '>':
-            s += line.strip()
+def kmer_comp():
+    kmer = {}
+    pros = itertools.product('ACGT', repeat=4)
+    for pro in pros:
+        x = ''.join(pro)
+        if x not in kmer:
+            kmer[x] = 0
 
-dict = {}
-pros = itertools.product('ACGT', repeat=4)
-for pro in pros:
-    x = ''.join(pro)
-    if x not in dict:
-        dict[x] = 0
+    with open('output.txt', 'w') as f:
+        for key in kmer.keys():
+            c = len(re.findall(r'(?={0})'.format(key), s))
+            f.write('{} '.format(str(c)))
 
-with open('output.txt', 'w') as f:
-    for key in dict.keys():
-        c = len(re.findall(r'(?={0})'.format(key), s))
-        f.write(str(c) + ' ')
+if __name__ == '__main__':
+    with open('data/rosalind_kmer.txt') as f:
+        s = ''
+        for line in f.readlines():
+            if line[0] != '>':
+                s += line.strip()
+    kmer_comp()
