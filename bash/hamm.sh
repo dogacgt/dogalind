3a4edb4a31412e8f9513b14fa13df5cd8c892183
dogacgt <doganuraydogan@gmail.com>
2021-05-03 01:44:52 +0300

    add hamm.sh

---
+++ bash/hamm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Counting Point Mutations
+
+eval $(awk '{ print "d"NR"="$1 }' data/rosalind_hamm.txt)
+length=${#d1}
+
+count=0
+for ((i=0 ; i<$length ; i++)); do
+	if [[ ${d1:$i:1} != ${d2:$i:1} ]]; then
+		((count++))
+	fi
+done
+echo $count
