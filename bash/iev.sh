bbd938dcedf4ed36a2eafe4ea1ac47baaeedd7cc
dogacgt <doganuraydogan@gmail.com>
2021-05-03 04:15:08 +0300

    add files

---
+++ bash/iev.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Calculating Expected Offspring
+
+vec=($(cat data/rosalind_iev.txt))
+prob=(1 1 1 .75 .5 0)
+
+sum=0
+for ((i=0 ; i<6 ; i++)); do
+	a=`echo ${prob[$i]} ${vec[$i]} | awk '{ print $1 * $2 * 2 }'`
+	sum=`echo $sum + $a | bc`
+done
+
+echo $sum
---
+++ bash/subs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#Finding a Motif in DNA
+
+eval $(awk '{ print "d"NR"="$1 }' data/rosalind_subs.txt)
+sl=${#d1}
+tl=${#d2}
+
+for ((i=0 ; i<$sl ; i++)); do
+	if [[ ${d1:$i:$tl} == ${d2} ]]; then
+		echo -n $(($i+1))' ' >> output.txt
+	fi
+done
